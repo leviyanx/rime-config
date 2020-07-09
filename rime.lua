9361c0082fdbee41e15924bb0fa33fbadc64763d
leviyan1998 <leviyan1998@gmail.com>
2020-07-09 17:16:06 +0800

    add 'datetime'

--- rime.lua
+++ rime.lua
@@ -5,7 +5,11 @@ function date_translator(input, seg)
    end
    if (input == "time") then
       --- Candidate(type, start, end, text, comment)
-      yield(Candidate("date", seg.start, seg._end, os.date("%H:%M"), "时间"))
+      yield(Candidate("date", seg.start, seg._end, os.date("%H:%M:%S"), "时间"))
+   end
+   if (input == "datetime") then
+      --- Candidate(type, start, end, text, comment)
+      yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d %H:%M:%S"), "日期&时间"))
    end
 end
 
