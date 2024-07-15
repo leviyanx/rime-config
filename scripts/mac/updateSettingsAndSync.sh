8777c488065a274966ea1e8bdb6b4971024383bb
Levi Yan <leviyanx@gmail.com>
2024-07-16 00:23:34 +0800

    feat: 添加一个用于在mac上拉取设置变化并同步用户数据、重新部署的脚本

---
+++ scripts/mac/updateSettingsAndSync.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ~/Library/Rime
+git pull
+sleep 10
+
+if [ -n "$(pgrep 'Squirrel')" ]; then 
+    cpu_usage=$(ps -p $(pgrep 'Squirrel') -o %cpu=); 
+    if [ "$(echo "$cpu_usage == 0" | bc -l)" -eq 1 ]; then 
+        # sync first(make sure the sync is performed)
+        DYLD_LIBRARY_PATH="/Library/Input Methods/Squirrel.app/Contents/Frameworks" "/Library/Input Methods/Squirrel.app/Contents/MacOS/Squirrel" --sync
+        sleep 10
+        DYLD_LIBRARY_PATH="/Library/Input Methods/Squirrel.app/Contents/Frameworks" "/Library/Input Methods/Squirrel.app/Contents/MacOS/Squirrel" --reload
+    fi; 
+fi
