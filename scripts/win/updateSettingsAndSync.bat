14540449da28248d12a9281dcb98bdfc7b296b08
Levi Yan <leviyanx@gmail.com>
2025-05-23 13:38:46 +0800

    edit(win/updateSettingsAndSync.bat): 版本升级到0.17.0

--- scripts/win/updateSettingsAndSync.bat
+++ scripts/win/updateSettingsAndSync.bat
@@ -1,4 +1,4 @@
 @echo off
 start "" /B /WAIT cmd /c "cd %USERPROFILE%\AppData\Roming\Rime && git pull"
-start "" /B /WAIT "C:\Program Files\Rime\weasel-0.16.1\WeaselDeployer.exe" /deploy
-start "" /B /WAIT "C:\Program Files\Rime\weasel-0.16.1\WeaselDeployer.exe" /sync
+start "" /B /WAIT "C:\Program Files\Rime\weasel-0.17.0\WeaselDeployer.exe" /deploy
+start "" /B /WAIT "C:\Program Files\Rime\weasel-0.17.0\WeaselDeployer.exe" /sync
